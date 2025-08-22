aba80950056a8f2081a000971651f6a6e569b42f
Bronya Zhang <3336939360@qq.com>
2025-08-23 02:20:12 +0800

    Add SpringAIChatApplication main class

---
+++ SpringAI-01-Chat/src/main/java/com/ikun/ai/SpringAIChatApplication.java
@@ -0,0 +1,12 @@
+package com.ikun.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAIChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAIChatApplication.class, args);
+    }
+}
